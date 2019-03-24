ab185113a428f8ea8e7cb6fc3432d30eb8da6989
MisaOgura <misa.ogura01@gmail.com>
2019-03-24 14:46:09 +0000

    Make sure to find the right layer in all torchvision models

--- torchscope/saliency/test/test_backprop.py
+++ torchscope/saliency/test/test_backprop.py
@@ -1,6 +1,10 @@
+import inspect
 import pdb
 import pytest
 
+from sys import stdout
+
+import scipy
 import torch
 import torchvision
 import torchvision.models as models
@@ -16,12 +20,19 @@ def test_set_model_to_eval_mode(mocker):
     model.eval.assert_called_once()
 
 
-def test_find_first_conv_layer(mocker):
-    model = models.vgg19()
-    mocker.spy(model, 'eval')
-    backprop = Backprop(model)
+def test_find_first_conv_layer_from_torchvision_models(mocker):
+    available_models = inspect.getmembers(models, inspect.isfunction)
 
-    target_layer = backprop.find_target_layer()
+    print()
+    for name, func in available_models:
+        print(f'Finding the first conv layer in model: {name}', end='\r')
+        stdout.write('\x1b[2K')
 
-    assert type(target_layer) == torch.nn.modules.conv.Conv2d
-    assert target_layer.in_channels == 3
+        model = func()
+        mocker.spy(model, 'eval')
+        backprop = Backprop(model)
+
+        target_layer = backprop.find_target_layer()
+
+        assert type(target_layer) == torch.nn.modules.conv.Conv2d
+        assert target_layer.in_channels == 3
