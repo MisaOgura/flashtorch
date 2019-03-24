99e4a8711d495b88b254fa05c1118439c01afbdd
MisaOgura <misa.ogura01@gmail.com>
2019-03-24 18:40:36 +0000

    Calculate gradient of target calss wrt the input image

--- torchscope/saliency/backprop.py
+++ torchscope/saliency/backprop.py
@@ -1,6 +1,6 @@
 """
 """
-
+import pdb
 import torch
 
 class Backprop:
@@ -17,7 +17,7 @@ class Backprop:
         self.target_layer_in_channels = 3
 
         self.gradients = None
-        self.register_hooks()
+        self.register_hook()
 
     def find_target_layer(self):
         """
@@ -36,10 +36,33 @@ class Backprop:
 
         return search(self.nodes)
 
-    def register_hooks(self):
+    def register_hook(self):
+        """
+        """
         def record_gradient(module, grad_in, grad_out):
             self.gradients = grad_in[0]
 
         first_conv_layer = self.find_target_layer()
         first_conv_layer.register_backward_hook(record_gradient)
 
+    def calculate_gradient(self, inputs, target_class):
+        """
+        """
+        # raw prediction values (logit) from the last linear layer
+        output = self.model(inputs)
+
+        self.model.zero_grad()
+
+        # Create a 2D tensor with shape (1, num_classes) and
+        # set all element to zero
+        target = torch.FloatTensor(1, output.size()[-1]).zero_()
+
+        # Set the element at target class index to be 1
+        target[0][target_class] = 1
+
+        # Calculate gradient of the value corresponding to the target class
+        # i.e output[target_class] w.r.t inputs
+
+        output.backward(gradient=target)
+
+        return self.gradients
--- torchscope/saliency/test/test_backprop.py
+++ torchscope/saliency/test/test_backprop.py
@@ -18,6 +18,7 @@ def model():
 
 
 def test_set_model_to_eval_mode(mocker, model):
+
     mocker.spy(model, 'eval')
     Backprop(model)
 
@@ -50,3 +51,55 @@ def test_register_hook(mocker, model):
 
     target_layer.register_backward_hook.assert_called_once()
 
+
+def test_zero_out_gradient(mocker, model):
+    backprop = Backprop(model)
+    mocker.spy(model, 'zero_grad')
+
+    target_class = 1
+    input_image = torch.zeros([1, 3, 224, 224],
+                              requires_grad=True,
+                              dtype=torch.float)
+
+    backprop.calculate_gradient(input_image, target_class)
+
+    model.zero_grad.assert_called_once()
+
+
+def test_calculate_gradient_of_target_class_only(mocker, model):
+    num_classes = 10
+    backprop = Backprop(model)
+
+    mocked_output = mocker.Mock()
+    mocked_output.size.return_value = (1, num_classes)
+
+    mocked_forward = mocker.patch.object(model,
+                                         'forward',
+                                         return_value=mocked_output)
+
+    target_class = 5
+    input_image = torch.zeros([1, 3, 224, 224],
+                              requires_grad=True,
+                              dtype=torch.float)
+
+    backprop.calculate_gradient(input_image, target_class)
+
+    expected_gradient_target = torch.FloatTensor(1, num_classes).zero_()
+    expected_gradient_target[0][target_class] = 1
+
+    args, kwargs = mocked_output.backward.call_args
+
+    assert torch.all(kwargs['gradient'].eq(expected_gradient_target))
+
+
+def test_calculate_gradient_wrt_inputs(mocker, model):
+    backprop = Backprop(model)
+
+    target_class = 1
+    input_image = torch.zeros([1, 3, 224, 224],
+                              requires_grad=True,
+                              dtype=torch.float)
+
+    gradient = backprop.calculate_gradient(input_image, target_class)
+
+    assert gradient.shape == input_image.shape
