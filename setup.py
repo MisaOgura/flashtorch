920fb7f34ec547610e82bc8b94d7389f6567cef3
Xestph <xestph@outlook.com>
2019-09-10 16:38:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md', 'r') as fh:
+with open('README.md', encoding="utf-8", mode='r') as fh:
     long_description = fh.read()
 
 version = '0.1.0'
