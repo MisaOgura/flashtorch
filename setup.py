cde45652270a3723906228b36c5a5330cb7bf6af
Misa Ogura <misa.ogura01@gmail.com>
2019-09-26 11:39:31 +0100

    Remove README depencendy from setup.py (#15)

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
+#!/usr/bin/env python
+""" FlashTorch is a feature visualization toolkit.
+It is built with PyTorch, for neural networks in PyTorch.
+
+Neural networks are often described as "black box". The lack of understanding
+on how neural networks make predictions enables unpredictable/biased models,
+causing real harm to society and a loss of trust in AI-assisted systems.
+
+Feature visualization is an area of research, which aims to understand how
+neural networks perceive images. However, implementing such techniques is often
+complicated.
+
+FlashTorch was created to solve this problem!
+
+You can apply feature visualization techniques such as saliency maps and
+activation maximization on your model, with as little as a few lines of code.
+
+It is compatible with pre-trained models that come with torchvision, and
+seamlessly integrates with other custom models built in PyTorch.
+
+All FlashTorch wheels on PyPI are distrubuted with the MIT License.
+"""
+
 from setuptools import setup, find_packages
 
+DOCLINES = (__doc__ or '').split("\n")
+long_description = "\n".join(DOCLINES[2:])
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
-version = '0.1.0'
+version = '0.1.1'
 
 setup(
     name='flashtorch',
